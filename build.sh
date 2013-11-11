912a77190e2952131bf67fe8b8e8b6142a856f89
tavert <tavert@gmail.com>
2013-11-11 03:31:01 -0800

    try to get some more debugging info here

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sh
 
-COIN_PROJECT=CoinUtils/trunk
+COIN_PROJECT=CoinUtils
+PROJECT_VERSION=trunk
 
 # install prerequisites using apt-cyg
 svn export https://github.com/zship/apt-cyg.git/trunk/apt-cyg apt-cyg
@@ -8,8 +9,8 @@ chmod +x apt-cyg
 ./apt-cyg install make mingw64-x86_64-gcc-g++ mingw64-x86_64-gcc-fortran
 
 # download COIN source
-svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT ~/$COIN_PROJECT
-cd ~/$COIN_PROJECT
+svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT/$PROJECT_VERSION ~/$COIN_PROJECT/$PROJECT_VERSION
+cd ~/$COIN_PROJECT/$PROJECT_VERSION
 # do svn update in case of connectivity trouble during checkout
 svn update --non-interactive --trust-server-cert
 
@@ -26,7 +27,14 @@ if test -f CoinUtils/src/CoinLpIO.hpp; then
 fi
 
 # build in the main source tree to avoid symlinking data files
+#mkdir build
+#cd build
 ./configure -C --host=x86_64-w64-mingw32
 make all -j4
 make install
+echo $PATH
+cd $COIN_PROJECT/test
+make unitTest.exe
+ldd unitTest.exe
+cd ../..
 make test

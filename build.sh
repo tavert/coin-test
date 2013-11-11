e65f7299f62606dfa3e36b41fd5f5666e4f8d1fe
tavert <tavert@gmail.com>
2013-11-11 01:09:28 -0800

    make a separate shell script for wercker build

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# stop on error
+set -e
+
+COIN_PROJECT=CoinBazaar/projects/GIMPy/trunk
+echo "CC is $CC, CXX is $CXX, COIN_PROJECT is $COIN_PROJECT"
+
+# download COIN source
+svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT ~/$COIN_PROJECT
+cd ~/$COIN_PROJECT
+# do svn update in case of connectivity trouble during checkout
+svn update --non-interactive --trust-server-cert
+
+# download third-party source
+for i in `ls ThirdParty/*/get.*`; do
+  cd `dirname $i`
+  ./`basename $i`
+  cd ../..;
+done
+
+# add a newline at end of CoinUtils/src/CoinLpIO.hpp until fixed
+#echo "" >> CoinUtils/src/CoinLpIO.hpp
+
+mkdir build
+cd build
+../configure -C
+make all -j4
+make install
+make test
+
+#    - curl -Sso wrap.rb https://gist.github.com/roidrage/5238585/raw
+#    - chmod +x wrap.rb && ./wrap.rb "make all -j4 > make.log 2>&1"
+#    - echo "CONFIGURE AND MAKE LOGS UPLOADED TO URL" && gist config.log make.log
+# should upload more of the subdirectory config.log's too once that works
+#    - make install && make test

25b7708aafd076e971edea6ace03a77f7a4c3265
tavert <tavert@gmail.com>
2013-11-11 04:03:50 -0800

    use cygwin compilers until BuildTools r3052 makes its way into CoinUtils, can't override CYGPATH_W until then

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ PROJECT_VERSION=trunk
 # install prerequisites using apt-cyg
 svn export https://github.com/zship/apt-cyg.git/trunk/apt-cyg apt-cyg
 chmod +x apt-cyg
-./apt-cyg install make mingw64-x86_64-gcc-g++ mingw64-x86_64-gcc-fortran
+./apt-cyg install make gcc-g++ gcc-fortran
+#./apt-cyg install make mingw64-x86_64-gcc-g++ mingw64-x86_64-gcc-fortran
 
 # download COIN source
 svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT/$PROJECT_VERSION ~/$COIN_PROJECT/$PROJECT_VERSION
@@ -26,9 +27,10 @@ mkdir -p CoinUtils/src
 echo "" >> CoinUtils/src/CoinLpIO.hpp
 
 # build in the main source tree to avoid symlinking data files
-#mkdir build
-#cd build
-./configure -C CYGPATH_W="cygpath -w" --host=x86_64-w64-mingw32
+mkdir build
+cd build
+../configure -C
+#CYGPATH_W="cygpath -w" --host=x86_64-w64-mingw32
 make all -j4
 make install
 #echo $PATH

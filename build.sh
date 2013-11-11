e3f58d9ba677a655989c1ab4311c96c44501770c
tavert <tavert@gmail.com>
2013-11-11 09:05:29 -0800

    do a cached windows build in line with linux version, negligible changes for mac. want to test if installing matplotlib from dmg would be faster but that will require messing around in local vm [ci skip]

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/sh
+# cygwin build
 
 COIN_PROJECT=CoinUtils
 PROJECT_VERSION=trunk
 
 # install prerequisites using apt-cyg
-svn export https://github.com/kou1okada/apt-cyg.git/trunk/apt-cyg /usr/local/bin/apt-cyg
+svn export --force https://github.com/ashumkin/apt-cyg.git/trunk/apt-cyg /usr/local/bin/apt-cyg
 chmod +x /usr/local/bin/apt-cyg
-apt-cyg -m http://mirrors.kernel.org/sourceware/cygwin/ install make gcc-g++ gcc-fortran
+mkdir -p $WERCKER_CACHE_DIR/apt-cyg
+apt-cyg -c $WERCKER_CACHE_DIR/apt-cyg install make gcc-g++ gcc-fortran
 #apt-cyg install make mingw64-x86_64-gcc-g++ mingw64-x86_64-gcc-fortran
 
-# download COIN source
-svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT/$PROJECT_VERSION ~/$COIN_PROJECT/$PROJECT_VERSION
-cd ~/$COIN_PROJECT/$PROJECT_VERSION
-# do svn update in case of connectivity trouble during checkout
-svn update --non-interactive --trust-server-cert
+# download COIN source, will do an update if already downloaded
+svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT/$PROJECT_VERSION $WERCKER_CACHE_DIR/$COIN_PROJECT/$PROJECT_VERSION
+cd $WERCKER_CACHE_DIR/$COIN_PROJECT/$PROJECT_VERSION
+# do svn update in case of connectivity trouble during checkout (seems to only be a travis problem)
+#svn update --non-interactive --trust-server-cert
 
 # download third-party source
 for i in `ls ThirdParty/*/get.*`; do
@@ -26,16 +28,22 @@ done
 mkdir -p CoinUtils/src
 echo "" >> CoinUtils/src/CoinLpIO.hpp
 
-# build in the main source tree to avoid symlinking data files
-mkdir build
+# might need to build in the main source tree to avoid symlinking data files with mingw compilers
+# uncomment one of the below cleanup lines if potential problems from past builds (config changes, etc)
+#rm build/config.cache || true
+#rm -rf build || true
+mkdir -p build
 cd build
 ../configure -C
 #CYGPATH_W="cygpath -w" --host=x86_64-w64-mingw32
 make all -j4
 make install
+make test
+
+
+
 #echo $PATH
 #cd $COIN_PROJECT/test
 #make unitTest.exe
 #ldd unitTest.exe
 #cd ../..
-make test

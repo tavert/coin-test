b7c47fc3417ecaf875999c6cdd99d3f606023c35
tavert <tavert@gmail.com>
2013-11-11 08:43:39 -0800

    need to install gist before I can use it

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ PROJECT_VERSION=trunk
 #  sudo ln -s $WERCKER_CACHE_DIR/apt-get /var/cache/apt/archives
 #fi
 sudo apt-get update -qq
-sudo apt-get install gfortran subversion
+sudo apt-get install gfortran subversion ruby
+gem install gist
 
 # download COIN source, will do an update if already downloaded
 svn co -q --non-interactive --trust-server-cert https://projects.coin-or.org/svn/$COIN_PROJECT/$PROJECT_VERSION $WERCKER_CACHE_DIR/$COIN_PROJECT/$PROJECT_VERSION
